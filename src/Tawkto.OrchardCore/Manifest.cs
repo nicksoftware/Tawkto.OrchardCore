2f0f2cbcbb2b98a0c6861f39d9e9d54bbe537011
NICOLAS MALULEKE <nicolusmaluleke@gmail.com>
2020-10-30 17:43:59 +0200

    changed manifest

--- src/Tawkto.OrchardCore/Manifest.cs
+++ src/Tawkto.OrchardCore/Manifest.cs
@@ -5,7 +5,7 @@ using System.Reflection;
     Name = "Tawk to Chat ",
     Author = "Nicolas Maluleke",
     Website = "https://nicksoftware.co.za",
-    Version = "1.0.0",
-    Description = "Monitor and chat with the visitors on your website,respond to support tickets and create a help center toempower customers to help themselves – all 100 % Free",
+    Version = "1.0.1",
+    Description = "Monitor and chat with the visitors on your website,respond to support tickets and create a help center to empower customers to help themselves – all 100 % Free",
     Category = "Messaging"
 )]
